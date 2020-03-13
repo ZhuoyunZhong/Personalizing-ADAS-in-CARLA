d9006af576114b1fdb29a7a225b2f3ef96ed0f24
Zhuoyun Zhong <zzy905954450@gmail.com>
2020-03-13 03:21:24 -0400

    Documented

--- agents/navigation/global_route_planner_dao.py
+++ agents/navigation/global_route_planner_dao.py
@@ -1,31 +1,19 @@
-# This work is licensed under the terms of the MIT license.
-# For a copy, see <https://opensource.org/licenses/MIT>.
-
-"""
-This module provides implementation for GlobalRoutePlannerDAO
-"""
-
 import numpy as np
 
 
 class GlobalRoutePlannerDAO(object):
     """
-    This class is the data access layer for fetching data
-    from the carla server instance for GlobalRoutePlanner
+    This class is the data access layer for
+    fetching data from the carla server instance
+    for GlobalRoutePlanner
     """
 
     def __init__(self, wmap, sampling_resolution=1):
-        """get_topology
-        Constructor
-
-        wmap    :   carl world map object
-        """
         self._sampling_resolution = sampling_resolution
-        self._wmap = wmap
+        self._wmap = wmap  # Carla world map object
 
     def get_topology(self):
         """
-        Accessor for topology.
         This function retrieves topology from the server as a list of
         road segments as pairs of waypoint objects, and processes the
         topology into a list of dictionary objects.
@@ -38,18 +26,24 @@ class GlobalRoutePlannerDAO(object):
                 path    -   list of waypoints separated by 1m from entry
                             to exit
         """
+        # To return the world's topology list
         topology = []
+
         # Retrieving waypoints to construct a detailed topology
+        # Each segment: tuple(carla.Waypoint, carla.Waypoint)
+        # representing the begin or end of a road
         for segment in self._wmap.get_topology():
             wp1, wp2 = segment[0], segment[1]
-            l1, l2 = wp1.transform.location, wp2.transform.location
+            p1, p2 = wp1.transform.location, wp2.transform.location
             # Rounding off to avoid floating point imprecision
-            x1, y1, z1, x2, y2, z2 = np.round([l1.x, l1.y, l1.z, l2.x, l2.y, l2.z], 0)
-            wp1.transform.location, wp2.transform.location = l1, l2
+            x1, y1, z1, x2, y2, z2 = np.round([p1.x, p1.y, p1.z, p2.x, p2.y, p2.z], 0)
+            wp1.transform.location, wp2.transform.location = p1, p2
+            # Save as a dictionary
             seg_dict = dict()
             seg_dict['entry'], seg_dict['exit'] = wp1, wp2
             seg_dict['entryxyz'], seg_dict['exitxyz'] = (x1, y1, z1), (x2, y2, z2)
             seg_dict['path'] = []
+            # Get path way points from begin to end according to sampling resolution
             endloc = wp2.transform.location
             if wp1.transform.location.distance(endloc) > self._sampling_resolution:
                 w = wp1.next(self._sampling_resolution)[0]
@@ -63,11 +57,11 @@ class GlobalRoutePlannerDAO(object):
 
     def get_waypoint(self, location):
         """
-        The method returns waypoint at given location
+        Returns a waypoint that can be located in an exact location
+        or translated to the center of the nearest lane
         """
-        waypoint = self._wmap.get_waypoint(location)
-        return waypoint
+        return self._wmap.get_waypoint(location)
 
     def get_resolution(self):
-        """ Accessor for self._sampling_resolution """
+        """Get sampling resolution"""
         return self._sampling_resolution
